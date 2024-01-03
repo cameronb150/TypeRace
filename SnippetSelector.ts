55dd16766e4866887fecf07677456baded3eec39
cameronb150 <108947351+cameronb150@users.noreply.github.com>
2024-01-03 14:09:57 -0600

    Create SnippetSelector.ts

---
+++ SnippetSelector.ts
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import SelectorButton from './SelectorButton';
+
+const SnippetSelector = ({films, chooseSnippet}) => {
+    const selections = [
+        {id: 1, title: 'Film Title'},
+        {id: 2, title: 'Description'},
+        {id: 3, title: 'Director'}
+    ];
+
+    const [whatToType, setWhatToType] = useState(null);
+
+    const chooseWhatToType = (selection) => setWhatToType(selection);
+
+    return (
+        <div>
+            {!whatToType ?
+            <div>
+                <h4>What would you like to type?</h4>
+                <SelectorButton buttonNames={selections} onSelection={chooseWhatToType}/>
+                </div>
+           : null }
+           {whatToType && films ? 
+           <div>
+            <h4>Choose One</h4>
+            <SelectorButton buttonNames={films} onSelection={chooseSnippet} selectionType={whatToType}/>
+            </div>
+           : null}
+
+        
+        </div>
+    )
+}
+
+export default SnippetSelector;
